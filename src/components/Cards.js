879b5ce83d1b17cfc8f97a2e9d421b816ee0e3e0
scantarbian <davidpamereanb@gmail.com>
2021-08-27 09:15:33 +0700

    crude faq menu

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { useState } from "react"
 
 import { Link } from 'gatsby'
 import { StaticImage } from "gatsby-plugin-image"
@@ -37,6 +38,33 @@ const benefitsList = [
   }
 ]
 
+const faqList = [
+  {
+    q: "OmahTI isinya cuman belajar doang ya?",
+    a: "Tentu enggak dong, di OmahTI kita juga bakalan ngeproker bareng ngelatih softskill, dan juga banyak kegiatan yg fun."
+  },
+  {
+    q: "Kalo gak ada pengalaman bisa gak sih daftar OmahTI?",
+    a: "Bisa banget!! Kita di OmahTI bakalan belajar dari nol bareng-bareng jadi gausah khawatir."
+  },
+  {
+    q: "Semisal aku anggota divisi ini aku boleh gak belajar ke divisi lain?",
+    a: "Oh boleh banget dong! Tinggal ikut aja pelatihan divisi yang lain kamu boleh tanya temen kamu tentang jadwalnya."
+  },
+  {
+    q: "Pelatihan di OmahTI di mana dan gimana?",
+    a: "Jadi nanti kamu akan dilatih sama kepala divisi kamu terkait dengan apa-apa yang perlu dipelajari. Pelatihannya dilaksanain di base OmahTI atau di gedung C atau tempat lain sesuai kesepakatan divisinya."
+  },
+  {
+    q: "Siapa aja yang boleh daftar OmahTI 2021?",
+    a: "Setiap mahasiswa ilmu komputer UGM angkatan 2019 dan 2020 yang berkeinginan untuk mendaftar"
+  },
+  {
+    q: "Apakah nantinya saya bisa kalah saing dengan angkatan atas yang mencoba di tahun kedua?",
+    a: "Tentu saja engga, buat angkatan 2021, kalian punya kesempatan yang lebih besar kok buat join di OTI dibanding angkatan 2020"
+  },
+]
+
 export const Welcome = () => {
   return (
     <Core>
@@ -57,8 +85,8 @@ export const Welcome = () => {
 export const WhatWeDo = () => {
   return (
     <Core>
-      <h1 className="font-black text-5xl leading-none
-      text-transparent bg-clip-text bg-gradient-to-b from-white to-purple-400">WHAT WE DO?</h1>
+      <h2 className="font-black text-5xl leading-none
+      text-transparent bg-clip-text bg-gradient-to-b from-white to-purple-400">WHAT WE DO?</h2>
       <p className="font-semibold text-center text-lg"> 
         Di OmahTI, kita bakal bersama-sama mengasah Hard Skill dan Soft Skill melalui program kerja yang pastinya seru banget! Penasaran? Gabung makanya!
       </p>
@@ -74,8 +102,8 @@ export const WhatWeDo = () => {
 export const Timeline = () => {
   return (
     <div className="flex flex-col items-center px-16">
-      <h1 className="font-black text-5xl leading-none
-      text-transparent bg-clip-text bg-gradient-to-bl from-white to-purple-400">RECRUITMENT TIMELINE</h1>
+      <h2 className="font-black text-5xl leading-none
+      text-transparent bg-clip-text bg-gradient-to-bl from-white to-purple-400">RECRUITMENT TIMELINE</h2>
       <StaticImage src="../assets/image/timeline.png" alt="Timeline Open Recruitment OmahTI UGM" className="w-2/3"/>
     </div>
   )
@@ -84,8 +112,8 @@ export const Timeline = () => {
 export const About = () => {
   return (
     <Core>
-      <h1 className="font-black text-5xl leading-none
-      text-transparent bg-clip-text bg-gradient-to-b from-white to-purple-400">ABOUT OMAHTI</h1>
+      <h2 className="font-black text-5xl leading-none
+      text-transparent bg-clip-text bg-gradient-to-b from-white to-purple-400">ABOUT OMAHTI</h2>
       <p className="font-semibold text-justify text-lg"> 
         Organisasi Mahasiswa Ahli Teknologi Informasi atau lebih dikenal OmahTI bermula pada awal September 2002 di Laboratorium Inovasi. 
         Kemudian Laboratorium ini berubah nama, sifat dan orientasi kegiatannya. 
@@ -98,8 +126,8 @@ export const About = () => {
 export const Benefits = () => {
   return (
     <Core>
-      <h1 className="font-black text-5xl leading-none
-      text-transparent bg-clip-text bg-gradient-to-br from-white to-purple-400">BENEFITS</h1>
+      <h2 className="font-black text-5xl leading-none
+      text-transparent bg-clip-text bg-gradient-to-br from-white to-purple-400">BENEFITS</h2>
       <div className="flex space-x-4">
         {benefitsList.map((benefit, index) => (
           <div key={index} className="flex flex-col items-center w-1/4 space-y-4">
@@ -113,10 +141,40 @@ export const Benefits = () => {
 }
 
 export const Faq = () => {
+  const [selected, setSelected] = useState(0)
+
   return (
     <Core>
-      <h1 className="font-black text-5xl leading-none
-      text-transparent bg-clip-text bg-gradient-to-br from-white to-purple-400">FREQUENTLY ASKED QUESTIONS</h1>
+      <h2 className="font-black text-5xl leading-none
+      text-transparent bg-clip-text bg-gradient-to-br from-white to-purple-400">FREQUENTLY ASKED QUESTIONS</h2>
+      <div className="flex w-full space-x-4">
+        <div className="w-1/3 flex flex-col space-y-2 ">
+          {
+            faqList.map((faq, index) => (
+              <button 
+                key={`question-${index}`} 
+                value={index} 
+                className={`cursor-pointer text-left p-2 border-b-2 border-transparent hover:border-yellow-600 
+                ${selected == index ? 'font-bold border-yellow-600' : null }`}
+                onClick={(e) => {setSelected(index); console.log(selected)}}
+              >
+                {faq.q}
+              </button>
+            ))
+          }
+        </div>
+        <div className="w-2/3">
+          {
+            faqList.filter((faq, index) => index == selected).map((faq, index) => (
+              <div key={`answer-${index}`} className="flex flex-col text-left space-y-2">
+                <h3 className="font-black text-3xl
+                text-transparent bg-clip-text bg-gradient-to-b from-white to-purple-400">{faq.q}</h3>
+                <p className="text-xl">{faq.a}</p>
+              </div>
+            ))
+          }
+        </div>
+      </div>
     </Core>
   )
 }
@@ -124,8 +182,8 @@ export const Faq = () => {
 export const ReadyToJoinUs = () => {
   return (
     <Core>
-      <h1 className="font-black text-5xl leading-none
-      text-transparent bg-clip-text bg-gradient-to-bl from-white to-purple-400">READY TO JOIN US?</h1>
+      <h2 className="font-black text-5xl leading-none
+      text-transparent bg-clip-text bg-gradient-to-bl from-white to-purple-400">READY TO JOIN US?</h2>
       <div className="flex space-x-4 items-center py-8">
         <StaticImage src="../assets/image/divisi-oti/datsci.png" alt="Data Science" className="w-12"/>
         <StaticImage src="../assets/image/divisi-oti/webdes.png" alt="Web Design" className="w-12"/>

c1afe5e13caf02d279557255cc374c9592200a45
scantarbian <davidpamereanb@gmail.com>
2021-08-28 15:43:03 +0700

    implement figma bg

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,10 +29,10 @@ const HomepageAlt = () => {
     },
   };
   return (
-    <div>
+    <div className="bg-gradient-to-b from-blue-900 via-indigo-900 to-black">
       <title>Open Recruitment OmahTI 2021</title>
-      <main className="static ">
-        <div className="flex flex-col pt-16 space-y-32 bg-gradient-to-b from-blue-900 via-indigo-900 to-black">
+      <main className="static bg-omahti bg-no-repeat bg-cover bg-center">
+        <div className="flex flex-col pt-16 space-y-32 ">
           <Welcome />
           <WhatWeDo />
           <Timeline />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+        'omahti': "url('background.png')"
+      }),
+    }
   },
   variants: {
     extend: {},

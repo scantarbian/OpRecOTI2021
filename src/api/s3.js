a6a45db0c67b3b461c16751af996439df88de738
Abiseno Arya P <abiseno.arya@gmail.com>
2021-09-05 03:04:05 +0700

    memperbaiki bug tapi boong :hankey:

--- src/api/s3.js
+++ src/api/s3.js
@@ -1,35 +1,35 @@
-import S3 from 'aws-s3';
+import S3 from "aws-s3";
 
 const config = {
   bucketName: process.env.S3_BUCKET,
   region: process.env.AWS_REGION,
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-}
+};
 
-export default function handler(req, res){
+export default function handler(req, res) {
   // only accept post requests
-  if (req.method !== 'POST') {
+  if (req.method !== "POST") {
     return res.status(405).end();
   } else {
-    console.log(res.body)
-    // if (!req.files || !req.files.file) {
-    //   return res.status(400).end();
-    // } else {
-    //   // get file
-    //   const file = req.files.file;
-    //   // get file name
-    //   const fileName = file.name;
-    //   // get file type
-    //   const fileType = file.mimetype;
-    //   // get file size
-    //   const fileSize = file.data.length;
-    //   // get file data
-    //   const fileData = file.data;
+    // console.log(req.body);
+    console.log(req.files[0]);
+    if (!req.files || !req.files[0]) {
+      return res.status(400).end();
+    } else {
+      // get file
+      const file = req.files[0];
+      // get file name
+      const fileName = file.originalname;
+      // get file type
+      const fileType = file.mimetype;
+      // get file size
+      const fileSize = file.size;
+      // get file data
+      const fileData = file;
 
-    //   console.log(fileName)
-    // }
-
+      console.log(fileName);
+    }
 
     // const S3Client = new S3({...config, dirName: division.dirName});
     // S3Client.uploadFile(data.file[0])
